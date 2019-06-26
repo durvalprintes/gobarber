43dbb9eb2309e1b2aa15cc4e38feb20829d51613
Durval Printes <durvalprintes@gmail.com>
2019-06-26 16:08:22 -0300

    Criação de modelo Usuario e loader de Modelos

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ mensagem: 'Hello World!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Durval Printes',
+    email: 'durvalprintes@gmail.com',
+    password_hash: '1234567890',
+  });
+  res.json(user);
 });
 
 export default routes;
