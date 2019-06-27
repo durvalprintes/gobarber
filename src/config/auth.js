6e833c53677b2c1c3022670dcc25906bdc35cba5
Durval Printes <durvalprintes@gmail.com>
2019-06-26 22:01:18 -0300

    Gerando hash e token

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '607e5a644e190a6f874fc26fcd1e1527',
+  expires: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Durval Printes',
-    email: 'durvalprintes@gmail.com',
-    password_hash: '1234567890',
-  });
-  res.json(user);
+  res.json({ mensagem: 'Welcome to goBarber!' });
 });
+
+routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
