ff1464885c9b6f5bc63772ce2058ce83b9b739e6
Durval Printes <durvalprintes@gmail.com>
2019-06-27 21:48:50 -0300

    Middlewares e Yup

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
@@ -10,7 +11,11 @@ routes.get('/', async (req, res) => {
 });
 
 routes.post('/users', UserController.store);
-
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
 
 export default routes;
